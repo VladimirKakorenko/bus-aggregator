e0ec5a4c66e559c298a5c3da7d1da909299f1032
Dav <dav08649@gmail.com>
2022-05-21 18:12:10 +0400

    feat: split webconfigs by versions

--- src/Web/Server/Client/configs/webpack.common.js
+++ src/Web/Server/Client/configs/webpack.common.js
@@ -1,18 +1,12 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const port = process.env.PORT || 3000;
-const mode = process.env.NODE_ENV || "development";
-
 module.exports = {
-    mode: mode,
-    entry: path.join(__dirname, '/src/index.tsx'),
+    entry: path.join(__dirname, '../src/index.tsx'),
     output: {
-        path: path.resolve(__dirname, '..', 'wwwroot'),
+        path: path.resolve(__dirname, '..', '..', 'wwwroot'),
         filename: 'bundle.js',
     },
-    devtool: 'inline-source-map',
     module: {
         rules: [
             {
@@ -34,25 +28,19 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public', 'index.html')
+            template: path.join(__dirname, '..', 'public', 'index.html')
         })
     ],
-    devServer: {
-        host: 'localhost',
-        port: port,
-        historyApiFallback: true,
-        open: true,
-    },
     resolve: {
         extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.css', '.tsx', '.ts'],
         modules: [
-            path.resolve(__dirname, "./"),
-            path.resolve(__dirname, "node_modules"),
+            path.resolve(__dirname, "../src/"),
+            path.resolve(__dirname, "../node_modules"),
         ],
         alias: {
-            '@static': path.resolve(__dirname, './src/static'),
-            '@shared': path.resolve(__dirname, './src/shared'),
-            '@core': path.resolve(__dirname, './src/core'),
+            '@static': path.resolve(__dirname, '../src/static'),
+            '@shared': path.resolve(__dirname, '../src/shared'),
+            '@core': path.resolve(__dirname, '../src/core'),
         },
     }
 };
