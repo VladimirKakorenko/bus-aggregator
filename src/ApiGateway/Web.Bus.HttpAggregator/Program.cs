d955d3af1f22835958b3fe0455bb121282a012b7
Dav <dav08649@gmail.com>
2022-05-23 20:48:42 +0400

    feat: init api gateway

---
+++ src/ApiGateway/Web.Bus.HttpAggregator/Program.cs
@@ -0,0 +1,17 @@
+namespace Web.Bus.HttpAggregator;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
---
+++ src/ApiGateway/Web.Bus.HttpAggregator/Startup.cs
@@ -0,0 +1,26 @@
+namespace Web.Bus.HttpAggregator;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.Map("/", c => c.Response.WriteAsync("value"));
+        });
+    }
+}
