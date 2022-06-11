5408fd81e79be8568d13b2a40c89c401134216be
Dav <dav08649@gmail.com>
2022-06-11 23:24:56 +0200

    feat: add script to tag and publish images

---
+++ src/publish_all_dockerhub.ps1
@@ -0,0 +1,20 @@
+Param(
+    [parameter(Mandatory=$false)][string]$sourceTag="linux-latest",
+    [parameter(Mandatory=$false)][string]$sourcePrefix="bus",
+    [parameter(Mandatory=$false)][string]$targetTag="latest",
+    [parameter(Mandatory=$false)][string]$targetPrefix="dav08649"
+)
+
+# Write-Host "Building images" -ForegroundColor Green
+# Invoke-Expression 'cmd /c "docker-compose build"'
+
+$images = ("adapter.api", "data.api", "webapigw", "webstatus", "webspa")
+
+foreach($image in $images) {
+    $sourceImage = "${sourcePrefix}/${image}:${sourceTag}";
+    $targetImage = "${targetPrefix}/${image}:${targetTag}";
+
+    Write-Host "Publishing $sourceImage into $targetImage" -ForegroundColor Green
+    Invoke-Expression 'cmd /c "docker tag $sourceImage $targetImage"'
+    Invoke-Expression 'cmd /c "docker push $targetImage"'
+}
